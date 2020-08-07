4c5780c5c54666ba281b1390d44fe3aff426e522
kevinraynaud <kraynaud@gmail.com>
2020-08-07 16:13:18 +0200

    argonaute card

--- src/components/argonaute.jsx
+++ src/components/argonaute.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Argonaute = (props) => {
     return (
     <div className="member-item">
+        <img src="https://res.cloudinary.com/dkrtmft3y/image/upload/v1596809141/casque_qhbuv1.png" alt=""/>
         <p>{props.name}</p>
         <button className="button"
          onClick={() => props.supprimerArgonaute(props.id)}>x</button>
